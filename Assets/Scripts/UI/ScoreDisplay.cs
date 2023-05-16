21dd5e0d6025d7dcaab408592d6ee239fd2a6c9c
zqj <582132116@qq.com>
2023-05-16 11:24:09 +0800

    feat: 分数管理器

---
+++ Assets/Scripts/UI/ScoreDisplay.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    static Text scoreText;
+
+    void Awake() => scoreText = GetComponent<Text>();
+
+    void Start()
+    {
+        ScoreManager.Instance.ResetScore();
+    }
+
+    public static void UpdateText(int score) => scoreText.text = score.ToString();
+    public static void ScaleText(Vector3 targetScale) => scoreText.rectTransform.localScale = targetScale;
+}
