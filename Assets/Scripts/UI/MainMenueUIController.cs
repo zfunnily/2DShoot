ba60225651451b63f4e5aea5cbe804a305a27f6d
zqj <582132116@qq.com>
2023-06-25 16:03:27 +0800

    feat: 主菜单界面

--- Assets/Scripts/UI/MainMenueUIController.cs
+++ Assets/Scripts/UI/MainMenueUIController.cs
@@ -5,26 +5,50 @@ using UnityEngine.UI;
 
 public class MainMenueUIController : MonoBehaviour
 {
-    [SerializeField] Button buttonStartGame;
+    [Header("==== CANVAS ====")]
+    [SerializeField] Canvas mainMenueCanvas;
+
+    [Header("==== BUTTONS ====")]
+    [SerializeField] Button buttonStart;
+    [SerializeField] Button buttonOptions;
+    [SerializeField] Button buttonQuit;
 
     void OnEnable()
     {
-        buttonStartGame.onClick.AddListener(OnStartGameButtonClick);
+        ButtonPressedBehaviour.buttonFunctionTable.Add(buttonStart.gameObject.name, OnButtonStartClick);
+        ButtonPressedBehaviour.buttonFunctionTable.Add(buttonOptions.gameObject.name, OnButtonOptionsClick);
+        ButtonPressedBehaviour.buttonFunctionTable.Add(buttonQuit.gameObject.name, OnButtonQuitClick);
     }
 
     void OnDisable()
     {
-        buttonStartGame.onClick.RemoveAllListeners();
+        ButtonPressedBehaviour.buttonFunctionTable.Clear();
     }
 
     void Start()
     {
         Time.timeScale = 1f;
         GameManager.GameState = GameState.Playing;
+        UIInput.Instance.SelectUI(buttonStart);
     }
 
-    void OnStartGameButtonClick()
+    void OnButtonStartClick()
     {
+        mainMenueCanvas.enabled = false;
         SceneLoader.Instance.LoadGamePlayScene();
+    }
+
+    void OnButtonOptionsClick()
+    {
+        UIInput.Instance.SelectUI(buttonOptions);
+    }
+
+    void OnButtonQuitClick()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit(); // 只在编译好的程序才能执行. 在unity程序中是不生效的.
+        #endif
     }
 }
